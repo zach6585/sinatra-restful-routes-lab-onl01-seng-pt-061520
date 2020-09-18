18f45515d949755fbd17435e926a8d2b679e5b6b
zachary mines <zmines@wesleyan.edu>
2020-09-18 19:15:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < Sinatra::Base
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
+    @recipe.update
   end 
   
   
