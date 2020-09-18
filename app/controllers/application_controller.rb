13689c7840d2af34e2d51c5bdefa5a26b6e16398
zachary mines <zmines@wesleyan.edu>
2020-09-18 19:02:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
   
   post '/recipes/new' do 
     @recipe = Recipe.create(params[:name], params[:ingredients], params[:cook_time])
-    redirect to "recipes/#{params[:id]}"
+    redirect to "recipes/#{@recipe.id}"
   end 
   
   get '/recipes/:id' do 
